0f5675fd4dbcb75d3d7bec5bb67638cb1482fc44
Umesh Yadav <120667524+umesh0027@users.noreply.github.com>
2026-03-23 13:42:07 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://rkgventuresgroup.onrender.com/api",
 });
 
 // Attach token automatically
